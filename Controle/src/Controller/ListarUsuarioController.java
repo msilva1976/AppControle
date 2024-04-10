d776694656e0c57ac8df8dd7e685e44b72974733
Marcos Carlos Da Silva <70917267+msilva1976@users.noreply.github.com>
2024-04-10 13:44:24 -0300

    config delete

--- Controle/src/Controller/ListarUsuarioController.java
+++ Controle/src/Controller/ListarUsuarioController.java
@@ -17,13 +17,18 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
+
+
 
 public class ListarUsuarioController implements Initializable {
 
+    protected static final UsuarioModel newvalue = null;
     @FXML    private Button btnaualizar;
     @FXML    private Button btncancela;
     @FXML    private Button btndeletar;
@@ -32,7 +37,10 @@ public class ListarUsuarioController implements Initializable {
     @FXML    private TableColumn<UsuarioModel,Long> clmid;
     @FXML    private TableColumn<UsuarioModel, String> clmnome;
     @FXML    private TableView<UsuarioModel> tabelausuario;
+    private UsuarioModel selecionado;
+
    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initTabel();
@@ -42,16 +50,31 @@ public class ListarUsuarioController implements Initializable {
         btncancela.setOnKeyPressed((KeyEvent)->{
             fechar();
         });
-        tabelausuario.getSelectionModel().selectedItemProperty().addListener(new ChangeListener(){
+        btndeletar.setOnMouseClicked((MouseEvent)->{
+            deleta();
+
+        });
+        btndeletar.setOnKeyPressed((KeyEvent)->{
+            deleta();
+
+        });
+        btnaualizar.setOnMouseClicked((KeyEvent)->{
+
+        });
+        btnaualizar.setOnMouseClicked((MouseEvent)->{
+
+        });
+        tabelausuario.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
 
             @Override
-            public void changed(ObservableValue arg0, Object arg1, Object arg2) {
-                UsuarioModel usuario = 
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+               selecionado = (UsuarioModel) newValue;
+               selecionado.mostausuario();
             }
-
-        
             
         });
+
+
     
        
     }
@@ -77,6 +100,21 @@ public class ListarUsuarioController implements Initializable {
         UsuarioDAO dao = new UsuarioDAO();        
         return FXCollections.observableArrayList(dao.geList()) ;
 
+    }
+    public void deleta(){
+        if (selecionado != null) {
+            UsuarioDAO dao = new UsuarioDAO();
+            dao.delete(selecionado);
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setHeaderText("Usuario deletado com sucesso!");
+            alert.showAndWait();
+            tabelausuario.setItems(atualizaTabela());
+        } else {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setHeaderText("Usuario não selecionado");
+            alert.showAndWait();
+        }
+        
     }
     
   
